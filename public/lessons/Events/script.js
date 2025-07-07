b5beb3e2f1ba59e458fffe9cde9b2e7262af1654
Guilherme Marques <guilhermemarques@Guilhermes-Mac-mini.local>
2025-07-07 20:59:59 -0300

    events added

--- public/lessons/Events/script.js
+++ public/lessons/Events/script.js
@@ -1,3 +1,16 @@
 const header = document.getElementById("title")
 const bg = document.getElementById("square")
 const buttonEvent = document.getElementById("button")
+
+
+title.addEventListener('click', function () {
+    title.style.color = "#F8F8F8"
+})
+
+bg.addEventListener('mousedown', function(){ 
+    bg.style.backgroundColor = "red"
+})
+
+buttonEvent.addEventListener('mouseout', function(){ 
+    buttonEvent.style.backgroundColor = "yellow"
+})
